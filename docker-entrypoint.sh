752981b558818ce435505bcd7eb6c08a54b87dc5
Kevin Lee <Kevin.lee@voomly.com>
2022-12-18 11:31:46 +0800

    env var test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,12 @@
 echo "Starting SSH ..."
 /usr/sbin/sshd
 
+# Get environment variables to show up in SSH session
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
+echo "============== ENV VARS ==================="
+printenv
+echo "==========================================="
+
 echo "Starting APP ..."
 npm run start
