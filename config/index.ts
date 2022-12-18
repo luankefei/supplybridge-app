17a7eb0ff9f2ffda7c8297265360fa6888f8c1b8
Kevin Lee <Kevin.lee@voomly.com>
2022-12-18 17:22:28 +0800

    ENV files goes out of source control to avoid potential security issues

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,7 @@
-const API_URL: string | undefined = process.env.NEXT_PUBLIC_API_URL
+import getConfig from 'next/config'
+const { publicRuntimeConfig } = getConfig()
 
+const API_URL: string | undefined = publicRuntimeConfig.NEXT_PUBLIC_API_URL
 export {
   API_URL,
 };
--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  }
 }
 
 module.exports = nextConfig
