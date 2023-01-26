20bdf6fb67dad527442aad4d2c5af0a5ff11e87d
saritob <muhammadali.hisar@gmail.com>
2023-01-26 15:12:02 +0500

    Header and Layout added

--- pages/sustainability/index.tsx
+++ pages/sustainability/index.tsx
@@ -3,6 +3,9 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick'
 import Image from "next/image";
 import styled from "styled-components";
+import dynamic from "next/dynamic";
+const Layout = dynamic(() => import("components/Layout"));
+const Header = dynamic(() => import("components/NewHeader"));
 export default function SliderPage() {
     var settings = {
       dots: true,
@@ -12,27 +15,42 @@ export default function SliderPage() {
       slidesToScroll: 1
     };
     return (
+      <Layout>
+      <Header title="Sustainability" />
       <StyledSlider {...settings}>
-       <Image src="/images/slide1.svg" width={1056} height={724} alt="slide1" />
-       <Image src="/images/slide2.svg" width={1056} height={724} alt="slide2" />
-       <Image src="/images/slide3.svg" width={1056} height={724} alt="slide3" />
-       <Image src="/images/slide4.svg" width={1056} height={724} alt="slide4" />
-       <Image src="/images/slide5.svg" width={1056} height={724} alt="slide5" />
-       <Image src="/images/slide6.svg" width={1056} height={724} alt="slide6" />
-       <Image src="/images/slide7.svg" width={1056} height={724} alt="slide7" />
-       <Image src="/images/slide8.svg" width={1056} height={724} alt="slide8" />
+       <Image src="/images/slide1.svg" width={1056} height={700} alt="slide1" />
+       <Image src="/images/slide2.svg" width={1056} height={700} alt="slide2" />
+       <Image src="/images/slide3.svg" width={1056} height={700} alt="slide3" />
+       <Image src="/images/slide4.svg" width={1056} height={700} alt="slide4" />
+       <Image src="/images/slide5.svg" width={1056} height={700} alt="slide5" />
+       <Image src="/images/slide6.svg" width={1056} height={700} alt="slide6" />
+       <Image src="/images/slide7.svg" width={1056} height={700} alt="slide7" />
+       <Image src="/images/slide8.svg" width={1056} height={700} alt="slide8" />
       </StyledSlider>
+  </Layout>
+     
     );
   }
 
   const StyledSlider=styled(Slider)`
    width: 1056px;
-   height: 724px;
-     margin: 10px auto;
+   height: 700px;
+     margin: 0px auto;
     .slick-dots li button:before {
     color: #08979C;
     font-size: 20px;
     line-height: 20px;
+    img{
+    @media (max-width: ${(props) => props.theme.size.laptop}) {
+    width: 100%;
+    height: 500px;
+  }  
+
+  @media (max-width: ${(props) => props.theme.size.tablet}) {
+    width: 100%;
+    height: 450px;
+  } 
+    }
 
   }
 
