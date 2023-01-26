d5d225f6de0f6ec7bb8154e9b0c39ad747577587
saritob <muhammadali.hisar@gmail.com>
2023-01-26 15:17:35 +0500

    conflicts resolved

--- pages/sustainability/index.tsx
+++ pages/sustainability/index.tsx
@@ -3,12 +3,9 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick'
 import Image from "next/image";
 import styled from "styled-components";
-<<<<<<< HEAD
 import dynamic from "next/dynamic";
 const Layout = dynamic(() => import("components/Layout"));
 const Header = dynamic(() => import("components/NewHeader"));
-=======
->>>>>>> da1a2037b7fb0e5184ef15a5b5f351dfacde33f2
 export default function SliderPage() {
     var settings = {
       dots: true,
@@ -18,7 +15,6 @@ export default function SliderPage() {
       slidesToScroll: 1
     };
     return (
-<<<<<<< HEAD
       <Layout>
       <Header title="Sustainability" />
       <StyledSlider {...settings}>
@@ -33,35 +29,17 @@ export default function SliderPage() {
       </StyledSlider>
   </Layout>
      
-=======
-      <StyledSlider {...settings}>
-       <Image src="/images/slide1.svg" width={1056} height={724} alt="slide1" />
-       <Image src="/images/slide2.svg" width={1056} height={724} alt="slide2" />
-       <Image src="/images/slide3.svg" width={1056} height={724} alt="slide3" />
-       <Image src="/images/slide4.svg" width={1056} height={724} alt="slide4" />
-       <Image src="/images/slide5.svg" width={1056} height={724} alt="slide5" />
-       <Image src="/images/slide6.svg" width={1056} height={724} alt="slide6" />
-       <Image src="/images/slide7.svg" width={1056} height={724} alt="slide7" />
-       <Image src="/images/slide8.svg" width={1056} height={724} alt="slide8" />
-      </StyledSlider>
->>>>>>> da1a2037b7fb0e5184ef15a5b5f351dfacde33f2
     );
   }
 
   const StyledSlider=styled(Slider)`
    width: 1056px;
-<<<<<<< HEAD
    height: 700px;
      margin: 0px auto;
-=======
-   height: 724px;
-     margin: 10px auto;
->>>>>>> da1a2037b7fb0e5184ef15a5b5f351dfacde33f2
     .slick-dots li button:before {
     color: #08979C;
     font-size: 20px;
     line-height: 20px;
-<<<<<<< HEAD
     img{
     @media (max-width: ${(props) => props.theme.size.laptop}) {
     width: 100%;
@@ -73,8 +51,6 @@ export default function SliderPage() {
     height: 450px;
   } 
     }
-=======
->>>>>>> da1a2037b7fb0e5184ef15a5b5f351dfacde33f2
 
   }
 
