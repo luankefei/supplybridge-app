15cc90e0914c9599e2c77e93738905d23e1043e6
seven <Seven@supplybridge.com>
2023-05-17 13:41:50 +0000

    [scout] fill random data in summary
    - display random L2 and its related L3 list
    - display random suppliers not related to L2
    - ensure `yarn run build` success

--- components/scout/Summary.tsx
+++ components/scout/Summary.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import useStore from "hooks/useStore";
 import {
    SummaryContainer,
@@ -6,13 +6,62 @@ import {
    SummaryTitleColumn,
    SummaryTitle,
    SummaryColumn,
+   SummarySpaceColumn,
    SummaryLabel,
-   SummarySubCategoryListContainer,
-   SummarySubCategoryList,
-   SummarySubCategoryItem,
+   SummaryCategoryListContainer,
+   SummaryCategoryList,
+   SummaryCategoryItem,
    SummaryTop10List,
    SummaryTop10ListHalf,
+   SummarySupplierContainer,
+   SummarySupplierTitle,
 } from "components/scout/Summary.styled";
+
+import demoData from "components/scout/summaryCategoryData";
+
+function demoRandomPick(obj: any): any {
+   if (!obj) return null;
+   if (Array.isArray(obj)) {
+      if (!obj.length) return null;
+      return obj[Math.floor(Math.random() * obj.length)];
+   } else {
+      return demoRandomPick(Object.keys(obj));
+   }
+}
+
+function determineSummary(filterData: any, suppliers: any) {
+   const summary: any = {};
+   // TODO: determine summary title by filterData.q
+   summary.title = demoRandomPick(demoData.L2L3);
+   summary.categories = demoData.L2L3[summary.title];
+   summary.lastQ = filterData.q;
+   // TODO: rank suppliers by revenue; currently pick the first 10
+   const selectedSuppliers = suppliers.slice(0, 10);
+   const selectedN = Math.floor(selectedSuppliers.length/2);
+   summary.suppliersA = selectedSuppliers.slice(0, selectedN);
+   summary.suppliersB = selectedSuppliers.slice(selectedN);
+   return summary;
+}
+
+const NullableImg = (props: any) => {
+   const { url } = props;
+   return url ? <img src={url} /> : null ;
+};
+
+const Supplier = (props: any) => {
+   return (
+      <SummarySupplierContainer>
+         <NullableImg url={props.logo} />
+         <SummarySupplierTitle>{props.name}</SummarySupplierTitle>
+      </SummarySupplierContainer>
+   );
+};
+
+const Suppliers = (props: any) => {
+   return props.data?.map(
+      (z: any, i: number) => <Supplier key={i} logo={z.logo} name={z.longName || z.name} />
+   );
+};
 
 export default function Summary() {
   const {
@@ -26,28 +75,38 @@ export default function Summary() {
     vehicleFuelTypes,
   } = useStore();
 
+  const [summary, setSummary] = useState(determineSummary(filterData, suppliers) || {});
+  useEffect(() => {
+     if (filterData.q === summary.lastQ) return;
+     setSummary(determineSummary(filterData, suppliers));
+  }, [filterData]);
+
   return (
      <SummaryContainer><div>
         <SummaryColumn><SummaryCategoryIcon /></SummaryColumn>
         <SummaryTitleColumn>
-           <SummaryLabel>You are searching for ...</SummaryLabel>
-           <SummaryTitle>Wheel</SummaryTitle>
+           <SummaryLabel>Suggest for you ...</SummaryLabel>
+           <SummaryTitle>{summary.title}</SummaryTitle>
         </SummaryTitleColumn>
-        <SummaryColumn>
-           <SummaryLabel>Categories</SummaryLabel><SummarySubCategoryList>
-              <SummarySubCategoryItem>a</SummarySubCategoryItem>
-              <SummarySubCategoryItem>b</SummarySubCategoryItem>
-              <SummarySubCategoryItem>c</SummarySubCategoryItem>
-           </SummarySubCategoryList><SummarySubCategoryListContainer>
-           </SummarySubCategoryListContainer>
-        </SummaryColumn>
-        <SummaryColumn>
+        <SummarySpaceColumn space={20}>
+           <SummaryLabel>Categories</SummaryLabel>
+           <SummaryCategoryListContainer><SummaryCategoryList>
+              {summary.categories?.map(
+                 (z: string, i: number) => (<SummaryCategoryItem key={i}><a title={z}>{z}</a></SummaryCategoryItem>)
+              )}
+           </SummaryCategoryList></SummaryCategoryListContainer>
+        </SummarySpaceColumn>
+        <SummarySpaceColumn space={30}>
            <SummaryLabel>Top 10 Suppliers</SummaryLabel>
            <SummaryTop10List>
-              <SummaryTop10ListHalf>a</SummaryTop10ListHalf>
-              <SummaryTop10ListHalf>b</SummaryTop10ListHalf>
+              <SummaryTop10ListHalf>
+                 <Suppliers data={summary.suppliersA}/>
+              </SummaryTop10ListHalf>
+              <SummaryTop10ListHalf>
+                 <Suppliers data={summary.suppliersB}/>
+              </SummaryTop10ListHalf>
            </SummaryTop10List>
-        </SummaryColumn>
+        </SummarySpaceColumn>
      </div></SummaryContainer>
   );
 }
---
+++ components/scout/summaryCategoryData.tsx
@@ -0,0 +1,12 @@
+const data: any = {
+   L2L3: {
+      'Steering Gear': ['HPS', 'C-EPS', 'SP-EPS', 'dP-EPS', 'B-EPS', 'Steer-by-Wire (Innovation)', ],
+      'Wheel': ['Steel', 'Aluminium', 'CFRP', ],
+      'HV Battery': ['NMC', 'NCA', 'LFP', 'Solid State (Innovation)', 'Slicon Based (Innovation)', ],
+      'Water Pump': ['Convensional', 'Electrical', ],
+      'Starter Battery': ['EFB', 'AGM', ],
+      'Display': ['LCD', 'LED', 'OLED', ],
+      'Airbag': ['Driver Airbag', 'Passanger Airbag', 'Knee Airbag', 'Curtain Airbag', 'Pedestrian Airbag', ],
+   }
+};
+export default data;
