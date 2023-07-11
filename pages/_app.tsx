5904620b9f2409950d12e9b95ddaab12fe0a981d
bensonz <mr.bz@hotmail.com>
2023-07-11 16:55:51 +0800

    autoformat app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,12 @@ import NextNProgress from "nextjs-progressbar";
 import { ToastContainer } from "react-toastify";
 
 import { theme } from "config/theme";
-import NavigateService from 'services/navigate';
+import NavigateService from "services/navigate";
 
-import "../styles/css_short_loading_animation.css"
-import "../styles/css_long_loading_animation.css"
+import "../styles/css_short_loading_animation.css";
+import "../styles/css_long_loading_animation.css";
 import "../styles/globals.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { push } = useRouter();
