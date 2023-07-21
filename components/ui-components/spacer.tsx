075426344c42db2b486e5b2eaa25a17dca80fd5b
bensonz <mr.bz@hotmail.com>
2023-07-21 18:55:05 +0800

    remove all

--- components/ui-components/spacer.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const SpacingVertical = styled.div<{ space: string }>`
-  height: ${(props) => props.space};
-`;
-export const SpacingHorizontal = styled.div<{ space: string }>`
-  width: ${(props) => props.space};
-`;
