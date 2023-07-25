2eaa05cdb95969c7554bf5c170810cec9c468615
bensonz <mr.bz@hotmail.com>
2023-07-25 14:11:46 +0800

    remove unused code

--- components/raw-material/chart/index.tsx
+++ components/raw-material/chart/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useEffect, useState } from "react";
 import { Grid, Card, IconButton, Stack, Box } from "@mui/material";
 import { Close } from "@mui/icons-material";
