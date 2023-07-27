5640f0ff3b92c376199e273c73d05947c39a84ab
bensonz <mr.bz@hotmail.com>
2023-07-28 00:45:58 +0800

    fixed tooltip and descriptions

---
+++ components/raw-material/descriptions.ts
@@ -0,0 +1,232 @@
+import { RawMaterialName } from "./constants";
+
+/**
+ * This is GPT generated from a this file:
+ * https://l1o0qw27z5.feishu.cn/sheets/HfrpsLqAEhGiTLt1ZjIck2kYnod
+ *
+ */
+export const RawMaterialDescriptions: Record<
+  RawMaterialName,
+  {
+    Description: string;
+    AutomotiveUse: string;
+  }
+> = {
+  Brent: {
+    Description:
+      "Brent Crude oil is a major benchmark price for purchases of oil worldwide. ",
+    AutomotiveUse: " refined into various fuels and products.",
+  },
+  "Natural gas": {
+    Description:
+      "A clean-burning fossil fuel that can be used as a fuel source for vehicles.",
+    AutomotiveUse:
+      " processed into CNG or LNG as fuel or facility energy source.",
+  },
+  Gasoline: {
+    Description:
+      "Gasoline is a fuel made from crude oil and other petroleum liquids. Gasoline is mainly used in vehicle engines.",
+    AutomotiveUse: " fuel",
+  },
+  Coal: {
+    Description:
+      "Coal is the major fossil fuel used for generating electricity worldwide. Coal-fired power generation is link with greehouse gas emissions.",
+    AutomotiveUse: " electricity generation, production of steel.",
+  },
+  "UK Gas": {
+    Description:
+      "Natural gas produced in the United Kingdom, is a national benchmark price.",
+    AutomotiveUse:
+      " processed into CNG or LNG as fuel or facility energy source.",
+  },
+  Ethanol: {
+    Description:
+      "A renewable fuel made from plant material, such as corn or sugarcane.",
+    AutomotiveUse: " fuel (Flexible Fuel Vehicles FFVs).",
+  },
+  Gold: {
+    Description:
+      "Gold a precious yellow metallic element, highly malleable and ductile, and not subject to oxidation or corrosion.",
+    AutomotiveUse: " electronics and Electrical contacts.",
+  },
+  Silver: {
+    Description:
+      "Silver is a chemical element. It is a lustrous white metal that is prized for its decorative beauty and electrical conductivity.",
+    AutomotiveUse: " Electronics and Electrical contacts.",
+  },
+  Copper: {
+    Description:
+      "Copper, a chemical element that is a reddish, extremely ductile metal and an excellent conductor of electricity and heat.",
+    AutomotiveUse:
+      " electrical wiring, batteries, electrical contacts, and motors.",
+  },
+  Steel: {
+    Description:
+      "Steel is an alloy of iron and carbon with improved strength and fracture resistance compared to other forms of iron.",
+    AutomotiveUse: " body, chassis, engine.",
+  },
+  "Iron Ore": {
+    Description:
+      "Iron ores are rocks and minerals from which metallic iron can be extracted. The primary use of iron ore is to make steel.",
+    AutomotiveUse: " see 'Steel'.",
+  },
+  Lithium: {
+    Description: "Lithium is a soft, silver-white light metal.",
+    AutomotiveUse: " batteries for EV, heat-resistant glass.",
+  },
+  Platinum: {
+    Description:
+      "Platinum is a pure, silver-toned metal, amongst the rarest in the world, one of the most stable metals and is resistant to corrosion, even at high temperatures.",
+    AutomotiveUse:
+      " catalytic converter, airbag initiators, hydrogen fuel cells.",
+  },
+  Titanium: {
+    Description:
+      "Titanium is a lightweight, high-strength, low-corrosion structural metal and is used in alloy form.",
+    AutomotiveUse:
+      " engine parts, exhaust system, body, EV battery (LTO), EV's underbody shield.",
+  },
+  "HRC Steel": {
+    Description:
+      "Hot-rolled coil (HRC) is the predominant finished steel form in the US domestic and global steel industry, and the foundation for many steel-based industrial products.",
+    AutomotiveUse: " frames, wheels.",
+  },
+  Cobalt: {
+    Description:
+      "Cobalt is a hard, lustrous, grey metal with a high melting point (1493\u00b0C).",
+    AutomotiveUse: " EV batteries, magnets, steel-belted radial tires.",
+  },
+  Lead: {
+    Description:
+      "Lead is a soft, malleable, ductile, bluish-white, dense metallic element.",
+    AutomotiveUse: " starter battery, paint.",
+  },
+  Aluminum: {
+    Description:
+      "Aluminum is the most abundant metallic element in Earth's crust and the most widely used nonferrous metal.",
+    AutomotiveUse:
+      " Wheels, Gauges, Rotors, Trunk Lids, Chassis, Body, Suspension System.",
+  },
+  Tin: {
+    Description:
+      "Tin is a silvery, malleable metal mainly used in the production of solder and to coat other metals to prevent corrosion.Automotive: Solder, Lead-Acid battery, Surface Treatment, EV Battery Anode.",
+    AutomotiveUse: "",
+  },
+  Zinc: {
+    Description:
+      "Zinc is an abundant metal, found in the Earth's crust, often used to galvanise other metals. Often used in die-casting alloys, castings, brass products, paints and batteries.",
+    AutomotiveUse: " Engine, Brake system, Power Steering System.",
+  },
+  Nickel: {
+    Description:
+      "Nickel is a silvery-white lustrous metal with a slight golden tinge, a hard and ductile transition metal.",
+    AutomotiveUse: " EV Battery, Spark Plugs, Exhaust System, Sensors.",
+  },
+  Molybdenum: {
+    Description:
+      "Molybdenum is a silvery-white metal that is ductile and highly resistant to corrosion. It has one of the highest melting points of all pure elements (2623\u00b0C).",
+    AutomotiveUse:
+      " Muffler, Engine System, production of high-strength steel alloys.",
+  },
+  Palladium: {
+    Description:
+      "Palladium is a lustrous white material, one of the six platinum-group metals.",
+    AutomotiveUse: " Catalytic Converter, Hydrogen Fuel Cell, MCUs, and PCBs.",
+  },
+  Rhodium: {
+    Description:
+      "Rhodium is a silver-white metallic element resistant to corrosion and highly reflective.",
+    AutomotiveUse: " Catalytic Converter, Headlamp.",
+  },
+  Neodymium: {
+    Description:
+      "Neodymium is the strongest permanent magnet material yet discovered.",
+    AutomotiveUse: " Magnet, Microphone, Speakers, Electric Motor.",
+  },
+  Tellurium: {
+    Description:
+      "Tellurium is a silvery white element with properties intermediate between those of metals and nonmetals.",
+    AutomotiveUse: " Copper Allow, Stainless steel, Solid State Battery (EV).",
+  },
+  "Iron Ore 62% fe": {
+    Description: "",
+    AutomotiveUse: " Copper Allow, Stainless steel, Solid State Battery (EV).",
+  },
+  Magnesium: {
+    Description:
+      "A silvery-white metal that ignites easily in air and burns with a bright light.",
+    AutomotiveUse:
+      " Steering Column, Seat Frames Brake Lines, Brake Pads, Magnesium Alloy.",
+  },
+  Gallium: {
+    Description:
+      "Gallium is a soft, silvery-white metal, similar to aluminum. It is an important component of many semiconductors. It is a useful silicon substitute for the electronics industry.",
+    AutomotiveUse:
+      " Semiconductor, LED, Radio communication, Infrared Radiation.",
+  },
+  Germanium: {
+    Description:
+      "Germanium is a semiconductor mainly used as a transistor in thousands of electronic applications.",
+    AutomotiveUse:
+      " Semiconductor, LED, Radio communication, Infrared Radiation.",
+  },
+  Manganese: {
+    Description:
+      "Manganese Ore is a hard, brittle, silvery metal, often found in minerals in combination with iron.",
+    AutomotiveUse:
+      " Steel and iron production, additive in unleaded gasoline, EV Battery.",
+  },
+  Indium: {
+    Description:
+      "Indium is a soft, silvery metal that is stable in air and water.",
+    AutomotiveUse:
+      " Touch screens, Solar panels, Ball Bearing coating, Windshield.",
+  },
+  Polyethylene: {
+    Description: "Polyethylene is a member of the family of polyolefin resins.",
+    AutomotiveUse:
+      " Glass-reinforced exterior parts, Plastic Fuel Tanks, Electrical Insulation, Packaging.",
+  },
+  Polyvinyl: {
+    Description:
+      "Polyvinyl is the world's third-most widely produced synthetic polymer of plastic (after polyethylene and polypropylene).",
+    AutomotiveUse:
+      " Interior upholstery, Door Panel, Dashboard, Wiring Harness.",
+  },
+  Polypropylene: {
+    Description:
+      "Polypropylene is a semi-crystalline, durable, and high-performance plastic polymer. It is highly resistant to chemicals and heat and highly moldable.",
+    AutomotiveUse:
+      " Bumper fascia, Instrument Panel, Engine Cover, Cable Insulation.",
+  },
+  "Kraft Pulp": {
+    Description:
+      "The kraft process is a process for conversion of wood into wood pulp, which consists of almost pure cellulose fibres, the main component of paper.",
+    AutomotiveUse: " Packaging.",
+  },
+  Rubber: {
+    Description:
+      "Natural rubber is high resilience, extremely waterproof, and stretchable material.",
+    AutomotiveUse: " Tire, Gasket, HVAC System, Hoses, O-ring.",
+  },
+  "Solar Energy Index": {
+    Description:
+      "The Solar Energy Index CFD tracks the performance of publicly traded companies in the solar energy sector as well as those businesses that do not produce energy but make most of their revenues by providing goods and services to the solar energy industry.",
+    AutomotiveUse: "",
+  },
+  "EU Carbon Permits": {
+    Description:
+      "Allowances for carbon emissions are first allocated considering EU directives for the maximum amount of greenhouse gases that can be emitted. Allowances for carbon emissions are then auctioned and traded.",
+    AutomotiveUse: "",
+  },
+  "Wind Energy Index": {
+    Description:
+      "The Wind Energy Index CFD tracks the performance of publicly traded companies in the wind energy sector as well as those businesses that do not produce energy but make most of their revenues by providing goods and services to the wind energy industry.",
+    AutomotiveUse: "",
+  },
+  "Soda Ash": {
+    Description:
+      "Soda ash, also known as sodium carbonate, is an alkali chemical which is mostly produced from sodium chloride and limestone.",
+    AutomotiveUse: " Glass.",
+  },
+};
