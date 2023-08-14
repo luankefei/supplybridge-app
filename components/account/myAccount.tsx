a0c010a6c83a26fa10e2706a0e65553919c655e7
bensonz <mr.bz@hotmail.com>
2023-08-14 11:21:07 +0800

    basic 1

--- components/account/myAccount.tsx
+++ components/account/myAccount.tsx
@@ -1,12 +1,43 @@
-import { Stack } from "@mui/material";
-import { TitleText } from "components/ui-components/text";
+import { Button, Divider, Grid, Input, Paper, Stack } from "@mui/material";
+import { SpacingVertical } from "components/ui-components/spacer";
+import { STextBody16, TitleText } from "components/ui-components/text";
 import { useTranslation } from "react-i18next";
 
 export default function MyAccount() {
-  const { t } = useTranslation();
+  const { t } = useTranslation("myAccount");
   return (
-    <Stack>
-      <TitleText>My Account</TitleText>
+    <Stack
+      width={"500px"}
+      height={"400px"}
+      bgcolor={"white"}
+      borderRadius={2}
+      m={1}
+      p={4}
+    >
+      <Stack direction={"row"} justifyContent={"space-between"}>
+        <TitleText>{t("myAccount")}</TitleText>
+        <Button variant="outlined">Log out</Button>
+      </Stack>
+      <SpacingVertical space="16px" />
+      <Divider />
+      <SpacingVertical space="16px" />
+      <Grid container spacing={2} alignItems={"center"}>
+        <Grid item xs={6}>
+          <STextBody16>Email</STextBody16>
+        </Grid>
+        <Grid item xs={6}>
+          <Input />
+        </Grid>
+        <Grid item xs={6}>
+          <STextBody16>Display Name</STextBody16>
+        </Grid>
+        <Grid item xs={6}>
+          <Input />
+        </Grid>
+      </Grid>
+      <SpacingVertical space="16px" />
+      <Divider />
+      <SpacingVertical space="16px" />
     </Stack>
   );
 }
