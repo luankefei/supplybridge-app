9a3818da547cc6b0cc66d37a7f3eb102847d3a9c
Edward Wo <edward.wo823@gmail.com>
2023-08-15 12:25:43 +0800

    feat: build a skeleton for file mgr

--- components/account/fileManagement.tsx
+++ components/account/fileManagement.tsx
@@ -1,12 +1,21 @@
 import { Stack } from "@mui/material";
 import { TitleText } from "components/ui-components/text";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { useUserFiles } from "requests/useUserFiles";
 
 export default function FileManagement() {
   const { t } = useTranslation();
+  const { getFiles, deleteFiles, downloadFile, uploadFile } = useUserFiles();
+
+  useEffect(() => {
+    getFiles();
+  });
   return (
     <Stack>
       <TitleText>File Management</TitleText>
     </Stack>
   );
 }
+
+// check if window obj is null, not null means client, do stuff here,
---
+++ requests/useUserFiles.ts
@@ -0,0 +1,120 @@
+import { toast } from "react-toastify";
+
+import { request } from "config/axios";
+import { usePersistentStore, useStore } from "hooks/useStore";
+import { appStatus } from "hooks/appStatus";
+
+export const useUserFiles = () => {
+  const {
+    // setCommodities,
+    // setRegions,
+    // setComponents,
+    // setSubRegions,
+    setUserFiles,
+  } = useStore();
+  // const { setAllSubRegions } = usePersistentStore();
+
+  const getFiles = async () => {
+    // how to pass userId
+    try {
+      const { data } = await request.get("/user/files");
+      console.log("get files: ", data);
+      // setUserFiles(data);
+    } catch (err: any) {
+      console.log("getting file error: ", err);
+    }
+  };
+
+  const deleteFiles = async (fileId: string) => {};
+
+  const downloadFile = async (fileId: string) => {};
+
+  const uploadFile = async () => {};
+
+  /*
+  const getCommodities = async () => {
+    try {
+      const { data } = await request.get("commodities");
+      setCommodities(data?.commodities);
+    } catch (err: any) {
+      toast.error(err.response.data.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+  };
+
+  const getComponents = async (commodityIds: number[]) => {
+    try {
+      if (commodityIds.length) {
+        const { data } = await request.get(
+          `components?commodities=${commodityIds.toString()}`
+        );
+        setComponents(data?.components);
+      } else {
+        setComponents([]);
+      }
+    } catch (err: any) {
+      toast.error(err.response.data.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+  };
+
+  const getRegions = async () => {
+    try {
+      const { data } = await request.get("regions");
+      setRegions(data.regions);
+    } catch (err: any) {
+      toast.error(err.response.data.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+  };
+
+  const getSubRegions = async (regionIds: number[]) => {
+    try {
+      if (regionIds.length) {
+        const { data } = await request.get(
+          `sub_regions?regions=${regionIds.toString()}`
+        );
+        setSubRegions(data.subRegions);
+      } else {
+        setSubRegions([]);
+      }
+    } catch (err: any) {
+      toast.error(err.response.data.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+  };
+  const getAllSubRegions = async () => {
+    try {
+      const promise = request.get(`sub_regions`);
+      appStatus.addPromise("getAllSubRegions", promise);
+
+      const { data } = await promise;
+      setAllSubRegions(data.subRegions);
+      return data.subRegions;
+    } catch (err: any) {
+      toast.error(err.response.data.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      return null;
+    }
+  };
+  */
+
+  return {
+    /*
+    getCommodities,
+    getComponents,
+    getRegions,
+    getSubRegions,
+    getAllSubRegions,
+    */
+    getFiles,
+    deleteFiles,
+    downloadFile,
+    uploadFile,
+  };
+};
